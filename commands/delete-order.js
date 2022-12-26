603f6cb98c680c51b27e33273933a1c58e629145
vakhid.digital <info@vakhid.digital>
2022-12-26 23:18:47 +0500

    Revert "bugfix"
    This reverts commit 0834ab4b638248c0454ce9d1f7f266b9eb302702.

---
+++ commands/delete-order.js
@@ -0,0 +1,36 @@
+/*CMD
+  command: delete-order
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+
+  ANSWER
+
+  <<KEYBOARD
+
+  KEYBOARD
+  aliases: 
+CMD*/
+
+const orderOwner = params;
+const admin = 469750202;
+const permittedList = [orderOwner, admin];
+
+if (permittedList.includes(request.from.id)) {
+  Api.sendMessage({
+    text: `✅${Libs.commonLib.getLinkFor(user)} -ning so'rovi yopildi, rahmat!`,
+    parse_mode: "markdown",
+  });
+  Api.deleteMessage({
+    message_id: request.message.message_id,
+  });
+} else {
+  Api.answerCallbackQuery({
+    callback_query_id: request.id,
+    text: "Siz sorovni yopishga haqdor emassiz, yolovchi yoki admindan sorang",
+    show_alert: true,
+  });
+}
